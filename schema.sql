fa723cafbee625b2e2d9009ac2ce867ef3930dc0
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-08-22 11:59:48 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,10 +5,10 @@ CREATE DATABASE vet_clinic;
 USE vet_clinic;
 
 CREATE TABLE animals (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name CHAR(30) NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg FLOAT
-)
+);
